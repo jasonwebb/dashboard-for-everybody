01bd36e71612e9b740b5aca47d388ba865e37304
Jason Webb <jwebb@accessible360.com>
2019-10-30 13:54:02 -0500

    Hiding points in live chart

--- dashboard/js/entry.js
+++ dashboard/js/entry.js
@@ -60,7 +60,8 @@ let dataObject = {
     data: [],
     backgroundColor: 'rgba(255, 99, 132, 0.2)',
     borderColor: 'rgba(255, 99, 132, 1)',
-    borderWidth: 1
+    borderWidth: 1,
+    pointRadius: 0
   }]
 };
 
